da53d8bac113b50afb7ae4958feaa83aaca0b1ae
Rodrigo Henrique Fonseca Estrela <rohenri0808@gmail.com>
2024-03-30 19:11:37 -0300

    Primeira tentativa de realizar o desafio de triângulos

---
+++ ExerciciosComplementares/Ex1.js
@@ -0,0 +1,24 @@
+// Escreva um programa que leia as medidas dos lados de um triângulo e escreva sua classificação de acordo com o tamanho dos seus lados:
+
+// Equilátero: todos os lados tem o mesmo tamanho
+// Isósceles: dois lados tem o mesmo tamanho, mas o terceiro é diferente
+// Escaleno: os três lados do triângulo tem tamanhos diferentes
+// Observação: é preciso verificar se os lados informados são realmente de um triângulo, ou seja, se a forma pode ser "fechada". Para isso, os lados da forma devem atender a TODAS as seguintes condições:
+
+// ladoA < (ladoB + ladoC)
+// ladoB < (ladoA + ladoC)
+// ladoC < (ladoA + ladoB)
+
+
+
+let resultado
+
+function tipoTriangulo (lado1, lado2, lado3){
+
+      if ((lado1 < lado2 + lado3 ) && (lado2 < lado1 + lado3 ) && lado3 < lado1 + lado2){
+          if ((lado1 === lado2) && (lado2 === lado3)){
+           resultado = document.querySelector('#resultado').innerHTML = 'Triângulo equilátero'
+          }
+      }
+
+}
