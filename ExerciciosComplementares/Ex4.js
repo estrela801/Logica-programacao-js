363b32cd3fab3ce1b4d70a1485be1bb5b2f73b35
Rodrigo Henrique Fonseca Estrela <rohenri0808@gmail.com>
2024-04-03 17:44:26 -0300

    Pensando sobre como manipular arrys

--- ExerciciosComplementares/Ex4.js
+++ ExerciciosComplementares/Ex4.js
@@ -1,2 +1,12 @@
 // Desenvolva um programa que permita a leitura dos nomes de 10 pessoas e armazene os nomes lidos em um vetor. Após isso, o programa deve pertmitir a leitura de mais um nome qualquer para ser efetuada uma busca entre os nomes inseridos anteriormente. O programa deve exibir a mensagem ACHEI caso encontre o nome e NÃO ACHEI caso não encontre o nome.
 
+
+
+function bucaNome (name){
+    {
+        nome: name
+    }
+    if (name){
+        console.log(true)
+    }
+}
