d69b8ba13f1d1b4e3901fb94d1ecf484a3453be6
Rodrigo Henrique Fonseca Estrela <rohenri0808@gmail.com>
2024-04-07 23:56:21 -0300

    Alteraçoes

--- ExerciciosComplementares/Ex4.js
+++ ExerciciosComplementares/Ex4.js
@@ -1,14 +1,13 @@
 // Desenvolva um programa que permita a leitura dos nomes de 10 pessoas e armazene os nomes lidos em um vetor. Após isso, o programa deve pertmitir a leitura de mais um nome qualquer para ser efetuada uma busca entre os nomes inseridos anteriormente. O programa deve exibir a mensagem ACHEI caso encontre o nome e NÃO ACHEI caso não encontre o nome.
 
-let nomes = document.querySelector('#name').value ;
+let name = document.querySelector('#name').value ;
 
-function ArmazenaNome(_name) {
-
-    nomes.push(document.querySelector("#name"))
+function ArmazenaNome(name) {
+	name.push(document.querySelector("#name"))
 	console.log(nomes);
 }
 
-function verificacaoNome() {
-	nomes.includes(document.querySelector("#VerificaNome"));
+function verificacaoNome(name) {
+	name.includes(document.querySelector("#VerificaNome"));
 	console.log(nomes);
 }
