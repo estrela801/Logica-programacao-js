b480e101a1992a633353911932262a9440576650
RodrigoHEstrela <rohenri0808@gmail.com>
2024-04-08 11:50:20 -0300

    Aprendendo sobre manipulações de array

--- ExerciciosComplementares/Ex4.js
+++ ExerciciosComplementares/Ex4.js
@@ -1,13 +1,24 @@
 // Desenvolva um programa que permita a leitura dos nomes de 10 pessoas e armazene os nomes lidos em um vetor. Após isso, o programa deve pertmitir a leitura de mais um nome qualquer para ser efetuada uma busca entre os nomes inseridos anteriormente. O programa deve exibir a mensagem ACHEI caso encontre o nome e NÃO ACHEI caso não encontre o nome.
 
-let name = document.querySelector('#name').value ;
+// let a = ['um', 1, true, ]
 
-function ArmazenaNome(name) {
-	name.push(document.querySelector("#name"))
-	console.log(nomes);
-}
 
-function verificacaoNome(name) {
-	name.includes(document.querySelector("#VerificaNome"));
-	console.log(nomes);
+// function ArmazenaNome(name) {
+	
+// 	console.log(name);
+// }
+
+// function verificacaoNome(name) {
+// 	name.includes(document.querySelector("#VerificaNome"));
+// 	console.log(name);
+// }
+
+
+
+const variavel = ['a', 'b', 'c', 'd', 'e', 'f']
+
+for (let i = 0; i < variavel.length; i++){
+
+	console.log(variavel[i]);
+
 }
