0a35360bfcc3d2edfb7793d217bfc6c4639f0ed5
RodrigoHEstrela <rohenri0808@gmail.com>
2024-04-15 11:49:09 -0300

    chore: alterações em alguns processoes e na screen

--- Scripts/Ex2.js
+++ Scripts/Ex2.js
@@ -1,25 +1,25 @@
-// const funcionario = document.querySelector('#funcionario');
-// const clienteVp = document.querySelector('#clienteVp');
-// const cliente = document.querySelector('#cliente');
-// const banana = document.querySelector('#banana');
-// const laranja = document.querySelector('#laranja');
-// const maca = document.querySelector('#maca');
-      
-const valor = banana + laranja + maca;
-console.log(valor)
+const laranja = document.querySelector('#laranjas').value * 3
+const maca = document.querySelector('#maca').value * 1
+const banana = document.querySelector('#banana').value *4
+const tipoDesconto = document.querySelector('#tipoDesconto')
 
-let mensagem
+const valor =  laranja + maca +
 
+function calculeValor (valor, tipoDesconto){
+   let msg
+      if(tipoDesconto === 'funcionario')
+      {
+         msg = valor - valor * 10/100
+      }
+       else if(tipoDesconto === 'vip')
+      {
+         msg = valor - valor * 5/100
+      }
+      else
+      {
+         msg = valor
+      }
+      return msg
 
-function Desconto(tipoFuncionario, banana, laranja, maca){
-
-   if (funcionario){
-      mensagem = valor * 10 /100
-      console.log(valor)
-   } else if (clienteVp){
-      mensagem = valor * 5 /100
-   } else {
-      mensagem = valor
-   }
-       
 }
+document.querySelector('#valorFinal').innerHTML = msg
