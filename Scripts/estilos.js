c4e18975b8bef34475621ec70a73fd2f87ff67a3
estrela801 <rohenri0808@gmail.com>
2024-05-07 09:14:19 -0300

    refact: automatizando a criação de links

---
+++ Scripts/estilos.js
@@ -0,0 +1,14 @@
+const links = [
+     {'rel' : 'stylesheet', 'href' : '/Styles/reset.css'},
+     {'rel' : 'stylesheet', 'href' : '/Styles/styles.css'},
+     {'rel' : 'stylesheet', 'href' : '/Styles/breadcrumbs.css'},
+     {'rel' : 'stylesheet', 'href' : '/Styles/responsividade.css'},
+
+]
+
+links.forEach(link => {
+    const l = document.createElement('link');
+    l.rel = link.rel;
+    l.href = link.href;
+    document.head.appendChild(l)
+})
