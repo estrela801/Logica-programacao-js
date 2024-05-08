5610c7685972b44f16e5eea49f28644b277b6dea
estrela801 <rohenri0808@gmail.com>
2024-05-08 11:18:38 -0300

    refactor: movaendo arquivos para outra pasta

---
+++ Aulas/operadores-atribuicao.js
@@ -0,0 +1,19 @@
+let numero = 10; 
+
+console.log(`O numero é inicialmente igual a ${numero}`);
+
+console.log(`Após reatribuir somando o número 10: ${numero += 10}`);
+
+console.log(`Reatribuindo e subtraindo o número 10: ${numero -= 10}`);
+
+console.log(`Reatribuindo e multiplicando o número 10: ${numero *= 10}`);
+
+console.log(`Reatribuindo e dividindo o número 10: ${numero /= 10}`);
+
+console.log(`Reatribuindo para o resto da divisão por 10: ${numero %= 10}`)
+
+console.log(`Incrementando o número em 1: ${++ numero}`)
+
+console.log(`Decrementando o número em 1: ${-- numero }`)
+
+console.log(`O número final é igual a ${numero}`);
---
+++ Aulas/operadores-comparacao.js
@@ -0,0 +1,11 @@
+const a = 'a', b = 'A';
+
+const condicao = a == b;
+
+if (condicao){
+    console.log('VERDADEIRO/CONCORDO');
+
+    
+} else{
+    console.log('FALSO/DISCORDO')
+}
